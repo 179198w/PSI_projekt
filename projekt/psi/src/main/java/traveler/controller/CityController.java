34520d40f9a0bc314442258c0236ffe84c9332a2
179198w <179198@student.pwr.wroc.pl>
2014-02-02 10:07:09 +0100

    a

--- projekt/psi/src/main/java/traveler/controller/CityController.java
+++ projekt/psi/src/main/java/traveler/controller/CityController.java
@@ -20,55 +20,55 @@ public class CityController {
 
 	@Inject
 	private CityService cityService;
-	
+
 	@Inject
 	private CountryService countryService;
-	
+
 	@RequestMapping("/lista-miast")
 	public String listCities(Model model) {
 		model.addAttribute("cities", cityService.listCities());
 		return "listCities";
 	}
-	
+
 	@RequestMapping(value = "/dodaj-miasto", method = RequestMethod.GET)
-	public String addCityForm(Model model) {
-		model.addAttribute("cityCommand", new CityCommand());
+	public String addCityForm(Model model, CityCommand cityCommand) {
+		model.addAttribute("cityCommand", cityCommand);
 		model.addAttribute("countries", countryService.listCountries());
 		return "addCity";
 	}
-	
+
 	@RequestMapping(value = "/dodaj-miasto", method = RequestMethod.POST)
-	public String addCity(Model model,@Valid CityCommand cityCommand, BindingResult result) {
+	public String addCity(Model model, @Valid CityCommand cityCommand,
+			BindingResult result) {
 		if (result.hasErrors()) {
-			return "addCity";
+			return addCityForm(model, cityCommand);
 		}
 		cityService.addCity(cityCommand);
 		return listCities(model);
 	}
-	
+
 	@RequestMapping(value = "/edytuj-miasto", method = RequestMethod.POST)
 	public String addCity(Model model, Long cityId) {
-		CityCommand city=cityService.getCity(cityId);
+		CityCommand city = cityService.getCity(cityId);
 		model.addAttribute("cityCommand", city);
 		model.addAttribute("countries", countryService.listCountries());
 		return "addCity";
 	}
-	
+
 	@RequestMapping(value = "/usun-miasto", method = RequestMethod.POST)
 	public String removeCountry(Model model, Long cityId) throws Exception {
 		cityService.removeCity(cityId);
 		return "redirect:/lista-miast";
 	}
-	
+
 	@RequestMapping(value = "/szukaj-miasto", method = RequestMethod.POST)
 	public String searchCities(Model model, String search) {
-		if(!search.isEmpty()){
+		if (!search.isEmpty()) {
 			model.addAttribute("cities", cityService.listCities(search));
-		}else{
+		} else {
 			model.addAttribute("cities", cityService.listCities());
 		}
 		return "listCities";
 	}
-	
-	
+
 }
